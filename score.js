556eab7dd8d44d82f5dfb9262d4abc333acb853b
bankarom <146808698+bankarom@users.noreply.github.com>
2024-02-16 18:50:52 +0530

    Add files via upload

--- score.js
+++ score.js
@@ -1,1 +1,15 @@
 // Iteration 8: Making scoreboard functional
+var urlParams = new URLSearchParams(window.location.search);
+console.log(urlParams);
+
+var score = urlParams.get("score");
+
+var scoreboard = document.getElementById("score-board");
+
+var playAgainButton = document.getElementById("play-again-button");
+
+scoreboard.innerHTML = score;
+
+playAgainButton.onclick = ()=>{
+    location.href = "./game.html";
+}
